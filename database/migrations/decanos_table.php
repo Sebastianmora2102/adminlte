06f3abb9fcea7c7d6bb3c8d001d0d1078a1a7450
Sebastianmora2102 <juniorfrias2120@gmail.com>
2023-06-04 20:47:35 -0500

    Initial commit

---
+++ database/migrations/decanos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('decanos', function (Blueprint $table) {
+            $table->char('codDecano', 2);
+            $table->char('nomDecano', 40);
+            $table->char('facultad', 2);
+            $table->primary('codDecano');
+            $table->foreign('facultad')
+            ->references('codFacultad')->on('facultades');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('decanos');
+    }
+};
